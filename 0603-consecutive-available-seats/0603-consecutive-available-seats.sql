1f8fb849ee3372eb832a5f105706142c3d1786f7
BADITHALA NAYAN PREETHAM <badithalanayanpreetham@gmail.com>
2026-09-16 21:49:59 +0530

    Update 0603-consecutive-available-seats.sql

--- 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -12,3 +12,15 @@ from (
       from Cinema
       where free=1) s
 where (dd=0 and ff=1) OR (dd=1 and ff=1) or (dd=1 and ff=0)      
+
+
+
+//instead of this we can write 
+      
+SELECT DISTINCT a.seat_id
+FROM Cinema a
+JOIN Cinema b 
+    ON ABS(a.seat_id - b.seat_id) = 1
+   AND a.free = 1 
+   AND b.free = 1
+ORDER BY a.seat_id ASC;
